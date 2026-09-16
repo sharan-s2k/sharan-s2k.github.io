1b60712d7734163b5dc4242da8de84e1abbeb793
sharan-s2k <91820450+sharan-s2k@users.noreply.github.com>
2026-09-15 17:59:27 -0700

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { viteSingleFile } from 'vite-plugin-singlefile'
+
+export default defineConfig({
+  plugins: [react(), viteSingleFile()],
+  build: {
+    cssCodeSplit: false,
+    assetsInlineLimit: 100000000,
+  },
+})
